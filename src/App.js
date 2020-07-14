9426b2c7433daf91c1272b96d7938cc44da4c554
Daniel Li <lidaniel225@gmail.com>
2020-07-14 12:32:06 -0700

    finished fetch function

--- src/App.js
+++ src/App.js
@@ -3,10 +3,47 @@ import './App.css';
 import Location from './Components/Location.js';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      city: "Chicago",
+      temperature: "",
+      temp_min: "",
+      temp_max: "",
+      // humidity: "",
+      // wind: "",
+    }
+    this.getWeather = this.getWeather.bind(this);
+  }
+
+  getWeather(city) {
+    const KEY = "8a5ef08ed5c0d2cb0672425f4f244986"
+    const link = `http://api.openweathermap.org/data/2.5/weather?q=${city.toLowerCase()}&appid=${KEY}`
+
+    fetch(link)
+      .then(function(response) {
+        return response.json()
+      })
+      .then(function(response) {
+        const location = response;
+        this.setState({
+          temperature: location.main.temp,
+          temp_min: location.main.temp_min,
+          temp_max: location.main.temp_max,
+        })
+      })
+  }
+
+  componentDidMount() {
+    this.getWeather(this.state.city);
+  }
+
   render() {
     return (
       <div className="App">
-        <Location />
+        {this.state.temperature}
+        {this.state.temp_min}
+        {this.state.temp_max}
       </div>
     );
   }
--- src/Components/Location.js
+++ src/Components/Location.js
@@ -1,16 +1,22 @@
 import React, {Component} from 'react';
 
-async function Location() {
-    const response = fetch('http://api.openweathermap.org/data/2.5/weather?q=Chicago&APPID=8a5ef08ed5c0d2cb0672425f4f244986'
-        , {mode: 'cors'})
+function Location() {
+    // const response = fetch('http://api.openweathermap.org/data/2.5/weather?q=Chicago&APPID=8a5ef08ed5c0d2cb0672425f4f244986', {mode: 'cors'})
+    //         .then(function(response) {
+    //             return response.json();
+    //         })
+    //         .then(function(response) {
+    //             console.log(response)
+    //         })
+    return (
+        fetch('http://api.openweathermap.org/data/2.5/weather?q=Chicago&APPID=8a5ef08ed5c0d2cb0672425f4f244986', {mode: 'cors'})
             .then(function(response) {
+                console.log(response.json())
                 return response.json();
             })
-            .then(function(response) {
-                console.log(response)
-            })
-    return (
-        response.json()
+            // .then(function(response) {
+            //     console.log(response)
+            // })
     )
 }
 
