3695c113ca491dd0a45bef8ed2d0cf179dbba54d
Daniel Li <lidaniel225@gmail.com>
2020-07-17 12:02:43 -0700

    Updates

---
+++ precache-manifest.d9ecdfeed5397a81f15099eff89bea6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff9434949d24a698534831669d39580a",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "59342688b73e387447b9",
+    "url": "/weather-app/static/css/main.5e28e4b7.chunk.css"
+  },
+  {
+    "revision": "f92d9cefddd339df80cb",
+    "url": "/weather-app/static/js/2.c378e63f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.c378e63f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59342688b73e387447b9",
+    "url": "/weather-app/static/js/main.82501706.chunk.js"
+  },
+  {
+    "revision": "6217e7c726a47d063da4",
+    "url": "/weather-app/static/js/runtime-main.8ce43dfd.js"
+  }
+]);
